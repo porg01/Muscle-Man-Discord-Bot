542f8d0c5e050649ed70e4a633ef85efac76ffa8
Milan Mehra <milanmdev@gmail.com>
2025-02-11 22:41:30 -0600

    a

--- verbs.py
+++ verbs.py
@@ -4,6 +4,7 @@ import nltk
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger_eng')
 
 
 def verb_check(text):
